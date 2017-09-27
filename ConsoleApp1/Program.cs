0bd3823e059eae59481a6884e9775562a6884b2c
Forcause <hrishtakyan@inbox.ru>
2017-09-27 21:55:52 +0300

    Create simple calculator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int user_choice = 0;
+            int numb_first;
+            int numb_second;
+            {
+                do
+                {
+                    Console.WriteLine("\nChoose number: \n1 - Multiplication \n2 - Division \n3 - Subtraction \n4 - Addition \n5-Exit");
+                    user_choice = Convert.ToInt32(Console.ReadLine());
+                    switch (user_choice)
+                    {
+                        case 1:
+                            Console.Write("Enter first number: ");
+                            numb_first = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter second number: ");
+                            numb_second = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Result: {0}", numb_first * numb_second);
+                            break;
+                        case 2:
+                            Console.Write("Enter first number: ");
+                            numb_first = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter second number: ");
+                            numb_second = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Result: {0}", numb_first / numb_second);
+                            break;
+                        case 3:
+                            Console.Write("Enter first number: ");
+                            numb_first = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter second number: ");
+                            numb_second = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Result: {0}", numb_first - numb_second);
+                            break;
+                        case 4:
+                            Console.Write("Enter first number: ");
+                            numb_first = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter second number: ");
+                            numb_second = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Result: {0}", numb_first + numb_second);
+                            break;
+                    }
+                } while (user_choice != 5);
+
+            }
+        }
+    }
+}
